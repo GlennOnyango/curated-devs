3e889e52bd423a4e285cde9fd0e27b9ca84ebe6c
GlennOnyango <glenntedd@gmail.com>
2024-09-29 00:27:27 +0300

    mobile responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,7 @@ module.exports = {
     extend: {
       colors: {
         blue_text: "#1F2C8F",
+        talk_to_us: "#EEEEEE",
       },
       fontFamily: {
         inter: ["Inter"],
@@ -13,7 +14,7 @@ module.exports = {
         12: "3.5rem",
       },
       animation: {
-        //rotate infinatly 
+        //rotate infinatly
         rotate_quad: "rotate 2s linear infinite",
       },
     },
